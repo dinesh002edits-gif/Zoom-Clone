5c39621c0961b0d388c7739266e58e17838702a7
dinesh002-su <dineshswain968@gmail.com>
2026-08-13 14:57:27 +0530

    add static not-found

---
+++ app/not-found.tsx
@@ -0,0 +1,6 @@
+export const dynamic = 'force-static'
+export const revalidate = false
+
+export default function NotFound() {
+  return <h1 style={{textAlign: 'center', marginTop: '100px'}}>404 | Page Not Found</h1>
+}
