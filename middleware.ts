cd3757dd6b4e7af203fbc02377ebf605370b915b
dinesh002-su <dineshswain968@gmail.com>
2026-08-13 13:06:54 +0530

    fix middleware clerk v5

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher([
+const isPublicRoute = createRouteMatcher([
   '/',
-  '/upcoming',
-  '/meeting(.*)',
-  '/previous',
-  '/recordings',
-  '/personal-room',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
 ])
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect()
+  if (!isPublicRoute(req)) auth().protect()
 })
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!_next|.*\\..*).*)', '/', '/(api|trpc)(.*)'],
 }
