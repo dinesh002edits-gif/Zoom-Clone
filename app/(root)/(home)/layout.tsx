8c062e7a7a498313978055f88a94c924c70965b3
dinesh002-su <dineshswain968@gmail.com>
2026-08-13 20:15:43 +0530

    fix: remove html body from nested layout

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,28 +1,19 @@
-import type { Metadata } from "next";
-import { ReactNode } from 'react'
-import Navbar from '@/components/Navbar';
-import Sidebar from '@/components/Sidebar';
-import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'YOOM',
-  description: 'A workspace for your team, powered by Stream',
+  title: "Yoom",
+  description: "Video Calling App",
+  icons: {
+    icon: "/icons/logo3.png",
+  },
 };
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="relative">
-      <StreamVideoProvider>
-        <Navbar />
-        <div className="flex">
-          <Sidebar />
-          <section className="flex min-h-screen flex-1 flex-col">
-            <div className="w-full">{children}</div>
-          </section>
-        </div>
-      </StreamVideoProvider>
+    <main className="relative"> 
+      {children}
     </main>
   );
 };
 
-export default RootLayout;
+export default HomeLayout;
