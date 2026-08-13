1f92752c8a89a79251b5f2d10959e2d9cd60dc83
dinesh002-su <dineshswain968@gmail.com>
2026-08-13 19:22:29 +0530

    fix: remove duplicate clerkprovider from home layout

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { ReactNode } from 'react'
-import { ClerkProvider } from '@clerk/nextjs'
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 import StreamVideoProvider from '@/providers/StreamClientProvider'
@@ -12,22 +11,17 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <ClerkProvider
-  afterSignInUrl="/"
-  afterSignUpUrl="/"
->
-      <main className="relative">
-        <StreamVideoProvider> 
-          <Navbar />
-          <div className="flex">
-            <Sidebar />
-            <section className="flex min-h-screen flex-1 flex-col">
-              <div className="w-full">{children}</div>
-            </section>
-          </div>
-        </StreamVideoProvider>
-      </main>
-    </ClerkProvider>
+    <main className="relative">
+      <StreamVideoProvider>
+        <Navbar />
+        <div className="flex">
+          <Sidebar />
+          <section className="flex min-h-screen flex-1 flex-col">
+            <div className="w-full">{children}</div>
+          </section>
+        </div>
+      </StreamVideoProvider>
+    </main>
   );
 };
 
